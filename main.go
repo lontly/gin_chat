c273f3e1b5ef81d2f476b303a6c33a0b8b8f8834
lontly <2925028830@qq.com>
2023-04-01 15:37:43 +0800

    commit

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "Gin_Chat/router"
+
+func main() {
+	r := router.Router()
+	r.Run()
+}
---
+++ models/UserBasic.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+//UserBasic继承gorm.Model
+type UserBasic struct {
+	gorm.Model
+	Identity     string
+	Name         string //用户名称
+	Password     string //用户密码
+	Phone        string //手机号
+	Email        string //邮箱
+	ClientIp     string //客户端Ip
+	ClientPort   string //客户端端口
+	LoginTime    time.Time //登录时间
+	LoginOutTime time.Time  `gorm:"login_out_time" json:"login_out_time"`//下线时间
+	Heartbeat    time.Time //心跳时间
+	IsLoginout   bool   //是否下线
+	DeviceInfo   string //设备相关信息
+}
+
+func (table *UserBasic) TableName() string {
+	return "user_basic"
+}
